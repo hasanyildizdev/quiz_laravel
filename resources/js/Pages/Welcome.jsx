3b29132b09ba9baaa2aaa5ac72147b160adcdfce
iyiapp <hasanyildiz.97@hotmail.com>
2023-04-30 14:17:45 +0300

    m

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -2,7 +2,6 @@ import { Link, Head } from '@inertiajs/react';
 import { useState, useEffect } from 'react';
 import '../../css/style.css';
 import music from './Quiz/Music';
-import Particles from "react-tsparticles";
 
 export default function Welcome(props, {auth}) {
 
@@ -42,84 +41,7 @@ export default function Welcome(props, {auth}) {
             alertMessage.remove();
         }, 3000);
     }
-
-    const particlesOptions = {
-        fps_limit: 60,
-        interactivity: {
-          detect_on: "canvas",
-          events: {
-            onclick: { enable: true, mode: "push" },
-            onhover: {
-              enable: true,
-              mode: "attract",
-              parallax: { enable: false, force: 60, smooth: 10 }
-            },
-            resize: true
-          },
-          modes: {
-            push: { quantity: 4 },
-            attract: { distance: 200, duration: 0.4, factor: 5 }
-          }
-        },
-        particles: {
-          color: { value: "#ffffff" },
-          line_linked: {
-            color: "#ffffff",
-            distance: 150,
-            enable: true,
-            opacity: 0.4,
-            width: 1
-          },
-          move: {
-            attract: { enable: false, rotateX: 600, rotateY: 1200 },
-            bounce: false,
-            direction: "none",
-            enable: true,
-            out_mode: "out",
-            random: false,
-            speed: 2,
-            straight: false
-          },
-          number: { density: { enable: true, value_area: 800 }, value: 80 },
-          opacity: {
-            anim: { enable: false, opacity_min: 0.1, speed: 1, sync: false },
-            random: false,
-            value: 0.5
-          },
-          shape: {
-            character: {
-              fill: false,
-              font: "Verdana",
-              style: "",
-              value: "*",
-              weight: "400"
-            },
-            image: {
-              height: 100,
-              replace_color: true,
-              src: "images/github.svg",
-              width: 100
-            },
-            polygon: { nb_sides: 5 },
-            stroke: { color: "#000000", width: 0 },
-            type: "circle"
-          },
-          size: {
-            anim: { enable: false, size_min: 0.1, speed: 40, sync: false },
-            random: true,
-            value: 5
-          }
-        },
-        polygon: {
-          draw: { enable: false, lineColor: "#ffffff", lineWidth: 0.5 },
-          move: { radius: 10 },
-          scale: 1,
-          type: "none",
-          url: ""
-        },
-        retina_detect: true
-      };
-
+    
     return (
         <>
             <Head title="Welcome" />
@@ -139,7 +61,9 @@ export default function Welcome(props, {auth}) {
                     </div>
                     <div className="row">
                         <div className='logo'>
+                            <h1 className=' text-white text-xl lg:text-4xl'>QUIZ</h1>
                             <img src="img/logo.png" alt="Logo" />
+                            <h1 className=' text-white text-xl lg:text-4xl'>TRADE</h1>
                         </div>
                     </div>
 
@@ -242,20 +166,6 @@ export default function Welcome(props, {auth}) {
 
                 </div>
             </div>
-
-            <Particles
-                id="tsparticles"
-                options={particlesOptions}
-                style={{
-                    position: "absolute",
-                    width: "100%",
-                    height: "100%",
-                    right: 0,
-                    top: 0,
-                    left: 0,
-                    bottom: 0,
-                }}
-            />
         </>
     );
 }
